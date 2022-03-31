6523b59d3161441d5f02838e4953aebc1ce40b8e
Ashokkumar <49029794+Ashok555@users.noreply.github.com>
2022-03-31 20:17:37 +0530

    Update FirstProject.java

--- FirstProject.java
+++ FirstProject.java
@@ -7,9 +7,13 @@ public class FirstProject {
 		System.out.println("Hello World");
 	}
 
+	public void  empname () {
+		System.out.println("Akon");
+	}
 	public static void main(String[] args) {
 		FirstProject ad = new FirstProject();
 		ad.empID();
+		ad.empname();
 		
 	
 	}
