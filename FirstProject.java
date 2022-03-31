aae2171045dd7027e6507c58c5264238e862f734
Ashokkumar <49029794+Ashok555@users.noreply.github.com>
2022-03-31 20:30:34 +0530

    Update FirstProject.java

--- FirstProject.java
+++ FirstProject.java
@@ -13,11 +13,15 @@ public class FirstProject {
 	public void  empnames () {
 		System.out.println("Akon is the best singer");
 	}
+	public void  empnam () {
+		System.out.println("Akon is the best singer in world");
+	}
 	public static void main(String[] args) {
 		FirstProject ad = new FirstProject();
 		ad.empID();
 		ad.empname();
 		ad.empnames();
+		ad.empnam();
 		
 	
 	}
