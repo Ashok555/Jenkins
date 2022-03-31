a1bcfc3bdfa03ed38a3ab25bd4ca45b6945ba3f9
Ashok555 <ashokkumargpm83@gmail.com>
2022-03-31 18:57:54 +0530

    first commit

---
+++ FirstProject.java
@@ -0,0 +1,17 @@
+package come.jen;
+
+public class FirstProject {
+	
+	
+	public void  empID () {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		FirstProject ad = new FirstProject();
+		ad.empID();
+		
+	
+	}
+
+}
